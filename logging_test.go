81cb284f510566054ade1fc40934656738550e44
Max <fredrik.wilhelm.reite@entur.org>
2025-05-20 12:27:50 +0200

    test: Added MarshalStack test

--- logging_test.go
+++ logging_test.go
@@ -9,17 +9,17 @@ func Example() {
 	// Default global logger
 	// logging.Info().Msg("Hello from the global logger!")
 
-	// Default child logger
+	// New child logger
 	// logger := logging.New()
 	// err := fmt.Errorf("oh no, an error")
 	// logger.Error().Err(err).Msg("An internal error occurred")
 
-	// Default child logger with stacktraced error
+	// New child logger with stacktraced error
 	// logger := logging.New()
 	// err := logging.NewStackTraceError("oh no, an error")
 	// logger.Error().Stack().Err(err).Msg("An internal error occurred!")
 
-	// Logger with custom writer
+	// Child logger with custom writer
 	w := zerolog.NewConsoleWriter()
 	w.NoColor = true
 	w.PartsExclude = []string{"timestamp"}
@@ -27,7 +27,7 @@ func Example() {
 	logger := logging.New(logging.WithWriter(w))
 	logger.Info().Msg("Hello from my new child logger!")
 
-	// Logger with custom writer and level
+	// Child logger with custom writer and level which won't be logged
 	logger = logging.New(logging.WithWriter(w), logging.WithLevel(zerolog.WarnLevel))
 	logger.Info().Msg("Hello from my new custom child logger!")
 
--- stacktrace_test.go
+++ stacktrace_test.go
@@ -1,6 +1,8 @@
 package logging
 
 import (
+	"fmt"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -34,9 +36,10 @@ func TestNewStack(t *testing.T) {
 			title:    "inner frame",
 			callback: inner,
 			expected: Expected{
-				file: "/stacktrace_test.go",
-				name: "func1",
-				line: 24,
+				frameN: 0,
+				file:   "/stacktrace_test.go",
+				name:   "func1",
+				line:   26,
 			},
 		},
 		{
@@ -46,7 +49,7 @@ func TestNewStack(t *testing.T) {
 				frameN: 1,
 				file:   "/stacktrace_test.go",
 				name:   "func2",
-				line:   29,
+				line:   31,
 			},
 		},
 	}
@@ -58,26 +61,110 @@ func TestNewStack(t *testing.T) {
 
 			stack := tmp.callback()
 			if len(stack.Frames)+1 < tmp.expected.frameN {
-				t.Errorf("specified frameN is not within vakud stack frame range\ngot: %d\nwant: <%d", tmp.expected.frameN, len(stack.Frames))
+				t.Fatalf("specified frameN is not within valid stack frame range\ngot: %d\nwant: <%d", len(stack.Frames), tmp.expected.frameN)
+			}
+
+			frame := stack.Frames[tmp.expected.frameN]
+
+			file := frame.File
+			line := frame.Line
+			name := frame.Func.Name()
+			i := strings.LastIndex(name, ".")
+			if i != -1 {
+				name = name[i+1:]
+			}
+
+			expect := tmp.expected.file
+			if expect != "" && !strings.HasSuffix(file, expect) {
+				t.Errorf("frame has file name suffix\ngot: %s\nwant: %s", file, expect)
+			}
+
+			expectI := tmp.expected.line
+			if expectI != 0 && line != expectI {
+				t.Errorf("frame has incorrect call line number\ngot: %d\nwant: %d", line, expectI)
+			}
+
+			expect = tmp.expected.name
+			if expect != "" && name != expect {
+				t.Errorf("frame has incorrect function name\ngot: %s\nwant: %s", name, expect)
+			}
+		})
+	}
+}
+
+func TestMarshalStack(t *testing.T) {
+	type Expected = []map[string]string
+
+	type Test struct {
+		title      string
+		stacktrace error
+		expected   Expected
+	}
+
+	var tests = []Test{
+		{
+			title:      "wrong error",
+			stacktrace: fmt.Errorf("wrong error"),
+			expected:   nil,
+		},
+		{
+			title:      "empty stacktrace error",
+			stacktrace: StackTraceError{},
+			expected:   []map[string]string{},
+		},
+		{
+			title:      "valid stacktrace error",
+			stacktrace: NewStackTraceError("valid error"),
+			expected: []map[string]string{
+				{
+					"file":     "/stacktrace_test.go",
+					"function": "TestMarshalStack",
+					"line":     "117",
+				},
+				{
+					"function": "tRunner",
+				},
+				{
+					"function": "goexit",
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		tmp := test
+		t.Run(tmp.title, func(t *testing.T) {
+			t.Parallel()
+
+			data := marshalStack(tmp.stacktrace)
+			if tmp.expected == nil {
+				if data != nil {
+					t.Fatalf("marshalled result value is incorrect\ngot: %+v\nwant: nil", data)
+				}
 			} else {
-				frame := stack.Frames[tmp.expected.frameN]
-
-				file := frame.File
-				line := frame.Line
-				name := frame.Func.Name()
-				i := strings.LastIndex(name, ".")
-				if i != -1 {
-					name = name[i+1:]
+				info, ok := data.([]map[string]string)
+				if !ok {
+					t.Fatalf("marshalled result is of the wrong type\ngot: %v\nwant: []map[string]string", reflect.TypeOf(data))
+				}
+				if len(tmp.expected) != len(info) {
+					t.Fatalf("stack info has incorrect length\ngot: %d\nwant: %d", len(info), len(tmp.expected))
 				}
 
-				if tmp.expected.file != "" && !strings.HasSuffix(file, tmp.expected.file) {
-					t.Errorf("frame has file name suffix\ngot: %s\nwant: %s", file, tmp.expected.file)
-				}
-				if tmp.expected.line != 0 && line != tmp.expected.line {
-					t.Errorf("frame has incorrect call line number\ngot: %d\nwant: %d", line, tmp.expected.line)
-				}
-				if tmp.expected.name != "" && name != tmp.expected.name {
-					t.Errorf("frame has incorrect function name\ngot: %s\nwant: %s", name, tmp.expected.name)
+				for i, frameInfo := range info {
+					expect := tmp.expected[i]["file"]
+					if expect != "" && !strings.HasSuffix(frameInfo["file"], expect) {
+						t.Errorf("frame info has incorrect file name suffix\ngot: %s\nwant: %s", frameInfo["file"], expect)
+					}
+
+					expect = tmp.expected[i]["line"]
+					if expect != "" && expect != frameInfo["line"] {
+						t.Errorf("frame info has incorrect call line number\ngot: %s\nwant: %s", frameInfo["line"], expect)
+					}
+
+					expect = tmp.expected[i]["function"]
+					if expect != "" && expect != frameInfo["function"] {
+						t.Errorf("frame info has incorrect function name\ngot: %s\nwant: %s", frameInfo["function"], expect)
+					}
 				}
 			}
 		})
