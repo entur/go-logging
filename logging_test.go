9e9506a81910655a26cd6237b61a9b6fffff5853
Max <fredrik.wilhelm.reite@entur.org>
2025-05-28 09:24:28 +0200

    feat: Added slog handler

--- logging_test.go
+++ logging_test.go
@@ -1,6 +1,8 @@
 package logging_test
 
 import (
+	"log/slog"
+
 	"github.com/entur/go-logging"
 )
 
@@ -28,6 +30,23 @@ func Example() {
 	logger = logging.New(logging.WithWriter(w), logging.WithLevel(logging.WarnLevel))
 	logger.Info().Msg("Hello from my new custom child logger!")
 
+	// Slog logger with zerolog handler. Inefficient, so should only be used sparingly
+	// if some other SDK is able to take a custom slog handler.
+	h := logging.NewSlogHandler(logging.WithWriter(w))
+	slogger := slog.New(h)
+	slogger = slogger.With(
+		slog.Int("some_int", 1),
+		slog.String("some_string", "huh"),
+		slog.Group("some_group",
+			slog.Int("some_nested_int", 3),
+			slog.Group("some_nested_group", 
+				slog.Float64("some_nested_nested_float", 100.0),
+			),
+		),
+	)
+	slogger.Info("Hello from my new custom slog handler")
+
 	// Output:
 	// INF Hello from my new child logger!
+	// INF Hello from my new custom slog handler some_group={"some_nested_group":{"some_nested_nested_float":100},"some_nested_int":3} some_int=1 some_string=huh
 }
